b3a5dbd73a1eea71b11bdc7338e7519911ba4781
keivn_wu <moontooth@gmail.com>
2019-10-26 19:17:50 +0800

    修改INDEX ITEM 排版 新增左側清單

--- WebApplication1/wwwroot/js/index/index.js
+++ WebApplication1/wwwroot/js/index/index.js
@@ -6,6 +6,6 @@ app.controller('IndexCtrl', function ($scope) {
         { src: 'https://banner.9splay.com/Global/SHOP/BANNER/20190920155337.jpg' }
     ];
     $scope.ItemList = [
-        1,2,3,4,5,6,7,8,9,10,11,12
+        1,2,3,4
     ];
 });
